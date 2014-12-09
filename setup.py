53101706d201eb50b61e14c505b04ca566955b8c
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-10 10:08:57 +1100

    needs a sudo. May have to revisit this

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def update_version_py():
             subprocess.call( ["cp", path + "pre-commit.sample", fname] )
             
             f = open(fname, "w")
-            f.write("#!/bin/sh \nmake install")
+            f.write("#!/bin/sh \nsudo make install")
             f.close()
              
     for i in ['post-checkout', 'post-commit', 'post-merge']: makeHook(i)
